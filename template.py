3f7219177ee9e431df5eff313a1da95df260a52a
sid <sidharthgoel55555@gmail.com>
2025-09-07 12:20:46 +0530

    folder structure created

---
+++ template.py
@@ -0,0 +1,36 @@
+import os
+from pathlib import Path
+
+project_name = "Chickdis"
+list_of_files = [
+    "github/workflows/gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb"
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        print(f"Creating directory: {filedir} for file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as fp:
+            pass
+        print(f"Creating empty file: {filepath}")
+    else:
+        print(f"File already exists: {filepath}, skipping creation.")
